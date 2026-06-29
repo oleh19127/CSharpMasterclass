e740876bd892cad0ee429e8aad775f7fc8365e54
oleh19127 <oleh19127@gmail.com>
2026-06-29 15:43:00 +0300

    feat: add method to calculate sum in a range

---
+++ CalculateSumOfNumbersBetween/Program.cs
@@ -0,0 +1,21 @@
+﻿static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+{
+	if (lastNumber < firstNumber)
+	{
+		return 0;
+	}
+
+	var sum = 0;
+	var counter = firstNumber;
+
+	while (counter <= lastNumber)
+	{
+		sum += counter;
+		counter++;
+	}
+
+	return sum;
+}
+
+Console.WriteLine(CalculateSumOfNumbersBetween(5, 10));
+
