9a4f58547ee41778cdbb7a66e089b8810b1200c1
oleh19127 <oleh19127@gmail.com>
2026-06-29 15:38:58 +0300

    feat: add character repetition method

---
+++ RepeatCharacter/Program.cs
@@ -0,0 +1,19 @@
+﻿string RepeatCharacter(char character, int targetLength)
+{
+	if (targetLength <= 1)
+	{
+		return character.ToString();
+	}
+
+	string result = character.ToString();
+
+	do
+	{
+		result += character;
+	}
+	while (result.Length < targetLength);
+
+	return result;
+}
+
+Console.WriteLine(RepeatCharacter('a', 2));
