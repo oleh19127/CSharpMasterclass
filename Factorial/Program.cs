03ed65cce2b2d584ba8f036bf50093050e07cc57
oleh19127 <oleh19127@gmail.com>
2026-06-29 15:37:00 +0300

    feat: add factorial calculation method

---
+++ Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿int CalculateFactorial(int number)
+{
+	if (number < 0)
+	{
+		throw new ArgumentException("The factorial of a negative number does not exist");
+	}
+
+	int result = 1;
+
+	for (int i = 1; i <= number; i++)
+	{
+		result *= i;
+	}
+
+	return result;
+}
+
+System.Console.WriteLine(CalculateFactorial(0));
