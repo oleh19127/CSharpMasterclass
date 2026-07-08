07e3b8bcb9d9bff50a6bafccb6b51478c78c752b
oleh19127 <oleh19127@gmail.com>
2026-07-09 01:08:20 +0300

    chore: adjust code indentation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,50 +13,50 @@ var userChoice = Console.ReadLine();
 
 if (string.Equals(userChoice, "A", StringComparison.OrdinalIgnoreCase))
 {
-	var result = AddNumbers(firstInput, secondInput);
-	Console.WriteLine($"The sum of the numbers is: {result}");
+    var result = AddNumbers(firstInput, secondInput);
+    Console.WriteLine($"The sum of the numbers is: {result}");
 }
 else if (string.Equals(userChoice, "S", StringComparison.OrdinalIgnoreCase))
 {
-	var result = SubtractNumbers(firstInput, secondInput);
-	Console.WriteLine($"Result of subtraction: {result}");
+    var result = SubtractNumbers(firstInput, secondInput);
+    Console.WriteLine($"Result of subtraction: {result}");
 }
 else if (string.Equals(userChoice, "M", StringComparison.OrdinalIgnoreCase))
 {
-	var result = MultiplyNumbers(firstInput, secondInput);
-	Console.WriteLine($"Result of multiplication: {result}");
+    var result = MultiplyNumbers(firstInput, secondInput);
+    Console.WriteLine($"Result of multiplication: {result}");
 }
 else
 {
-	Console.WriteLine("Invalid choice.");
+    Console.WriteLine("Invalid choice.");
 }
 
 int MultiplyNumbers(int firstNumber, int secondNumber)
 {
-	return firstNumber * secondNumber;
+    return firstNumber * secondNumber;
 }
 
 int SubtractNumbers(int firstNumber, int secondNumber)
 {
-	return firstNumber - secondNumber;
+    return firstNumber - secondNumber;
 }
 
 int AddNumbers(int firstNumber, int secondNumber)
 {
-	return firstNumber + secondNumber;
+    return firstNumber + secondNumber;
 }
 
 int GetInput(string message)
 {
-	Console.Write(message);
+    Console.Write(message);
 
-	int number;
+    int number;
 
-	while (!int.TryParse(Console.ReadLine(), out number))
-	{
-		Console.WriteLine("Invalid input. Please enter a real number.");
-		Console.Write(message);
-	}
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Invalid input. Please enter a real number.");
+        Console.Write(message);
+    }
 
-	return number;
+    return number;
 }
