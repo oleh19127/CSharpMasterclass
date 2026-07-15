33879d94bd83475c41ad3f33f60a727edfd79ce8
oleh19127 <oleh19127@gmail.com>
2026-07-15 16:57:59 +0300

    feat: implement file I/O and Single Responsibility Principle in Names project
    - Practice reading and writing text files using System.IO
    - Apply Single Responsibility Principle (SRP) by separating validation, formatting, and data access logic
    - Implement StringTextualRepository for data persistence
    - Use LINQ and char methods for complex string validation

---
+++ BreakingSRP/Program.cs
@@ -0,0 +1,80 @@
+﻿var names = new Names();
+var path = new NamesFileBuilder().BuildFilePath();
+var stringTextualRepository = new StringTextualRepository();
+if (File.Exists(path))
+{
+	Console.WriteLine("Names file already exists. Loading names.");
+	var stringsFromFile = stringTextualRepository.Read(path);
+	names.AddNames(stringsFromFile);
+}
+else
+{
+	Console.WriteLine("Names file does not yet exist.");
+	names.AddName("John");
+	names.AddName("not a valid name");
+	names.AddName("Claire");
+	names.AddName("definitely not a valid name");
+
+	Console.WriteLine("Saving names to a file.");
+	stringTextualRepository.Write(path, names.All);
+}
+Console.WriteLine(new NamesFormatter().Format(names.All));
+Console.ReadKey();
+class NamesFileBuilder
+{
+	public string BuildFilePath()
+	{
+		return "names.txt";
+	}
+}
+class NamesValidator
+{
+	public bool IsValid(string name)
+	{
+		return name.Length >= 2 && name.Length < 25 && char.IsUpper(name[0]) && name.All(char.IsLetter);
+	}
+}
+
+class StringTextualRepository
+{
+	private static readonly string Separator = Environment.NewLine;
+	public List<string> Read(string filePath)
+	{
+		var fileContents = File.ReadAllText(filePath);
+		return fileContents.Split(Separator).ToList();
+
+	}
+	public void Write(string filePath, List<string> strings)
+	{
+		File.WriteAllText(filePath, string.Join(Separator, strings));
+	}
+}
+
+class NamesFormatter
+{
+	public string Format(List<string> names)
+	{
+		return string.Join(Environment.NewLine + "------" + Environment.NewLine, names);
+	}
+}
+class Names
+{
+	public List<string> All { get; } = new List<string>();
+	private readonly NamesValidator _namesValidator = new NamesValidator();
+
+	public void AddNames(List<string> stringsFromFile)
+	{
+		foreach (var name in stringsFromFile)
+		{
+			AddName(name);
+		}
+	}
+
+	public void AddName(string name)
+	{
+		if (_namesValidator.IsValid(name))
+		{
+			All.Add(name);
+		}
+	}
+}
